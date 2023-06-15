04b030faa23566e4199d31770959b22135a2054e
Mehdi Şahin <sahinmehdi23@gmail.com>
2023-06-15 15:25:30 +0300

    add quiz view

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
+<script setup>
+import { RouterView } from "vue-router";
+import QuizesView from "./views/QuizesView.vue";
+</script>
+
 <template>
-  <div></div>
+  <div class="container">
+    <RouterView />
+  </div>
 </template>
+
+<style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+</style>
