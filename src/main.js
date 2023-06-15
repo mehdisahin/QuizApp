1b493e62108dfefc32d9b4523f68e115eba18046
Mehdi Şahin <sahinmehdi23@gmail.com>
2023-06-15 12:23:30 +0300

    add card and questions

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/index";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import "./assets/main.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
