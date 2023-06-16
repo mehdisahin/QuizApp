35062dd340036248bd872d93f99cd0feef246256
Mehdi Şahin <sahinmehdi23@gmail.com>
2023-06-16 16:29:11 +0300

    add gsap package and bug fix

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { RouterView } from "vue-router";
-import QuizesView from "./views/QuizesView.vue";
+// import QuizesView from "./views/QuizesView.vue";
 </script>
 
 <template>
   <div class="container">
+    <!-- <QuizesView /> -->
     <RouterView />
   </div>
 </template>
